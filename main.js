f74a18010a0fdc187df0e995775eb4f95cece52b
Marvin <maples4000@googlemail.com>
2024-04-03 10:30:50 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let wochentag = 0;
+  console.log(wochentag);
+
+while (wochentag <= 7 ) {
+
+    if (wochentag === 1) {
+        console.log(`Der ${wochentag}. Tag ist der Montag`);
+    } else if (wochentag === 2) {
+        console.log(`Der ${wochentag}. Tag ist der Dienstag`);
+    } else if (wochentag === 3) {
+        console.log(`Der ${wochentag}. Tag ist der Mittwoch`);
+    } else if (wochentag === 4) {
+        console.log(`Der ${wochentag}. Tag ist der Donnerstag`);
+    } else if (wochentag === 5) {
+        console.log(`Der ${wochentag}. Tag ist der Freitag`);
+    } else if (wochentag === 6) {
+        console.log(`Der ${wochentag}. Tag ist der Samstag`);
+    } else if (wochentag === 7) {
+        console.log(`Der ${wochentag}. Tag ist der Sonntag`);
+    } else {
+        console.log ("Falscher Wert")
+    }
+    wochentag++;
+}
