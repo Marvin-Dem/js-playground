fa19b55276cc406a8233941e78ddde44334d0547
Marvin <maples4000@googlemail.com>
2024-04-03 13:05:26 +0200

    Move logic to seperate function

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-let wochentag = 0;
-  console.log(wochentag);
+let wochentag = 9;
 
-while (wochentag <= 7 ) {
+function firsttry(weekdayIndex) {
 
-    if (wochentag === 1) {
-        console.log(`Der ${wochentag}. Tag ist der Montag`);
-    } else if (wochentag === 2) {
-        console.log(`Der ${wochentag}. Tag ist der Dienstag`);
-    } else if (wochentag === 3) {
-        console.log(`Der ${wochentag}. Tag ist der Mittwoch`);
-    } else if (wochentag === 4) {
-        console.log(`Der ${wochentag}. Tag ist der Donnerstag`);
-    } else if (wochentag === 5) {
-        console.log(`Der ${wochentag}. Tag ist der Freitag`);
-    } else if (wochentag === 6) {
-        console.log(`Der ${wochentag}. Tag ist der Samstag`);
-    } else if (wochentag === 7) {
-        console.log(`Der ${wochentag}. Tag ist der Sonntag`);
+    if (weekdayIndex === 1) {
+        return `Der ${weekdayIndex}. Tag ist der Montag`;
+    } else if (weekdayIndex === 2) {
+        return `Der ${weekdayIndex}. Tag ist der Dienstag`;
+    } else if (weekdayIndex === 3) {
+        return `Der ${weekdayIndex}. Tag ist der Mittwoch`;
+    } else if (weekdayIndex === 4) {
+        return `Der ${weekdayIndex}. Tag ist der Donnerstag`;
+    } else if (weekdayIndex === 5) {
+        return `Der ${weekdayIndex}. Tag ist der Freitag`;
+    } else if (weekdayIndex === 6) {
+        return `Der ${weekdayIndex}. Tag ist der Samstag`;
+    } else if (weekdayIndex === 7) {
+        return `Der ${weekdayIndex}. Tag ist der Sonntag`;
     } else {
-        console.log ("Falscher Wert")
+        return "Falscher Wert"
     }
-    wochentag++;
-} 
+}
+let result = firsttry(wochentag)
+console.log(result);
