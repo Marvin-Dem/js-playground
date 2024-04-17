1e83100b4f0445a1025169c073ae08abd2f6baf9
Marvin <maples4000@googlemail.com>
2024-04-17 12:21:25 +0200

    Finished the remaining tasks of "Atomare Bausteine"

---
+++ main.js
@@ -0,0 +1,7 @@
+import moment from 'moment';
+const date = moment().format();
+console.log(date);
+
+import {circle} from "./circle"
+
+console.log(circle())
--- practice.ts
+++ practice.ts
@@ -6,93 +6,101 @@ myInteGer = Math.sqrt(2);
 type MyObject = {
     name: string;
     amount: number;
-}
+};
 let myObject: MyObject = {
     name: "Johnny",
     amount: 23,
-}
+};
 console.log("Hello world");
 
-function logHelloWorld(){
-    console.log ("Hello world");
+function logHelloWorld() {
+    console.log("Hello world");
 }
 logHelloWorld();
 
-function logHello(person: string){
+function logHello(person: string) {
     console.log(`Hello ${person}`);
 }
 logHello("Marvin");
 
-function tellAge(name: string, age: number){
+function tellAge(name: string, age: number) {
     return `${name}, der alte Sack, ist ${age} Jahre alt.`;
 }
 console.log(tellAge("Philipp", 30));
 
 type Gender = "male" | "female";
-function tellGender(gender: Gender){
+function tellGender(gender: Gender) {
     if (gender === "male") {
         console.log("Your gender is male.");
-        }
-    else if (gender === "female") {
+    } else if (gender === "female") {
         console.log("Your Gender is female.");
     }
 }
 tellGender("male");
 tellGender("female");
 
-function isZero(nummer:number){
-    if (nummer === 0){
+function isZero(nummer: number) {
+    if (nummer === 0) {
         console.log("The number is 0.");
-    }
-    else {
+    } else {
         console.log("The number is not 0.");
     }
 }
 isZero(0);
 isZero(12);
 
-function compareNumbers(num1: number, num2: number){
-    if (num1<num2){
+function compareNumbers(num1: number, num2: number) {
+    if (num1 < num2) {
         console.log("num1 is smaller than num2.");
-    }
-    else if (num1>num2){
+    } else if (num1 > num2) {
         console.log("num1 is bigger than num2");
-    }
-    else if (num1===num2){
+    } else if (num1 === num2) {
         console.log("num1 and num2 are equal.");
     }
 }
-compareNumbers(3,7);
-compareNumbers(9,5);
-compareNumbers(4,4);
+compareNumbers(3, 7);
+compareNumbers(9, 5);
+compareNumbers(4, 4);
 
-const numberArray: number[] = [453, 32, 13425, 69];
-function getBiggestNumber(numberArray: number[]){   
-    if (numberArray.length === 0) {
+try {
+    const numberArray: number[] = [];
+    function getBiggestNumber(numberArray: number[]) {
+        if (numberArray.length === 0) {
             throw "Must pass at least one number!";
-    }
-    let bigNumber = 0;
-    for (let num of numberArray){
-        if (num>bigNumber){
-            bigNumber = num;
         }
-    } 
-    return bigNumber;
-} 
-let biggestNumber = getBiggestNumber(numberArray);
-console.log (`The biggest number is ${biggestNumber}.`);
+        let bigNumber = 0;
+        for (let num of numberArray) {
+            if (num > bigNumber) {
+                bigNumber = num;
+            }
+        }
+        return bigNumber;
+    }
+    let biggestNumber = getBiggestNumber(numberArray);
+    console.log(`The biggest number is ${biggestNumber}.`);
+} catch (error) {
+    console.error(error);
+}
 
 type Post = {
     title: string;
     description: string;
-}
-function longestDescription(posts:Post[]) {
+};
+const posts: Post[] = [
+    { title: "First post", description: "Short Description." },
+    { title: "Second Post", description: "This is the longest description." },
+    { title: "Third Post", description: "Longer Description." },
+];
+function longestDescription(posts: Post[]) {
     let longestDescription = posts[0];
     for (let post of posts) {
         if (post.description.length > longestDescription.description.length) {
             longestDescription = post;
         }
-    } 
+    }
     return longestDescription;
 }
-longestDescription 
+const post = longestDescription(posts);
+console.log(
+    `The post "${post.title}" has the longest description, which is saying: "${post.description}"`
+);
