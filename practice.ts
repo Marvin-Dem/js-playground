83476e9c884ea4d068403a790cf12bb51d7a1919
Marvin <maples4000@googlemail.com>
2024-04-17 15:18:21 +0200

    Updated version of the "Atomare Bausteine" task (finished and reviewed).

--- practice.ts
+++ practice.ts
@@ -1,13 +1,13 @@
 const myFinalVar = "hello";
-let overrideableVar;
-overrideableVar = "world";
+let myOverrideableVar;
+myOverrideableVar = "world";
 let myInteGer: number;
 myInteGer = Math.sqrt(2);
 type MyObject = {
     name: string;
     amount: number;
 };
-let myObject: MyObject = {
+const myObject: MyObject = {
     name: "Johnny",
     amount: 23,
 };
@@ -32,7 +32,7 @@ type Gender = "male" | "female";
 function tellGender(gender: Gender) {
     if (gender === "male") {
         console.log("Your gender is male.");
-    } else if (gender === "female") {
+    } else {
         console.log("Your Gender is female.");
     }
 }
@@ -53,8 +53,8 @@ function compareNumbers(num1: number, num2: number) {
     if (num1 < num2) {
         console.log("num1 is smaller than num2.");
     } else if (num1 > num2) {
-        console.log("num1 is bigger than num2");
-    } else if (num1 === num2) {
+        console.log("num1 is bigger than num2.");
+    } else {
         console.log("num1 and num2 are equal.");
     }
 }
@@ -62,22 +62,22 @@ compareNumbers(3, 7);
 compareNumbers(9, 5);
 compareNumbers(4, 4);
 
-try {
-    const numberArray: number[] = [];
-    function getBiggestNumber(numberArray: number[]) {
-        if (numberArray.length === 0) {
-            throw "Must pass at least one number!";
-        }
-        let bigNumber = 0;
-        for (let num of numberArray) {
-            if (num > bigNumber) {
-                bigNumber = num;
-            }
-        }
-        return bigNumber;
+function getBiggestNumber(numberArray: number[]) {
+    if (numberArray.length === 0) {
+        throw "Must pass at least one number!";
     }
-    let biggestNumber = getBiggestNumber(numberArray);
+    let bigNumber = 0;
+    for (let num of numberArray) {
+        if (num > bigNumber) {
+            bigNumber = num;
+        }
+    }
+    return bigNumber;
+}
+try {
+    let biggestNumber = getBiggestNumber([12, 69, 13425, 88]);
     console.log(`The biggest number is ${biggestNumber}.`);
+    getBiggestNumber([]);
 } catch (error) {
     console.error(error);
 }
@@ -102,5 +102,5 @@ function longestDescription(posts: Post[]) {
 }
 const post = longestDescription(posts);
 console.log(
-    `The post "${post.title}" has the longest description, which is saying: "${post.description}"`
+    `The post "${post.title}" has the longest description, which is saying: "${post.description}."`
 );
