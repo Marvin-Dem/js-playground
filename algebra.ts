0ffae8c8b4f33199b591c827b9e7b9f7629bc6d2
Marvin <maples4000@googlemail.com>
2024-04-18 17:10:52 +0200

    Finished the "Algebra" task.

--- algebra.ts
+++ algebra.ts
@@ -1,15 +1,51 @@
-type rectangle = {
+export type Rectangle = {
     height: number;
     width: number;
 };
-type cuboid = {
+export type Cuboid = {
     height: number;
     width: number;
     depth: number;
 };
 
-export function rectangleArea() {}
+export function rectangleArea(rectangles: Rectangle[]) {
+    if (rectangles.length === 0) {
+        throw "The list of rectangles cannot be empty!";
+    }
+    let biggestArea = 0;
+    for (let rectangle of rectangles) {
+        const area = rectangle.height * rectangle.width;
+        if (area > biggestArea) {
+            biggestArea = area;
+        }
+    }
+    return biggestArea;
+}
 
-export function rectangleDiagonal() {}
+export function rectangleDiagonal(rectangles: Rectangle[]) {
+    if (rectangles.length === 0) {
+        throw "The list of rectangles cannot be empty!";
+    }
+    let longestDiagonal = 0;
+    for (let rectangle of rectangles) {
+        const diagonal = Math.hypot(rectangle.height, rectangle.width);
+        if (diagonal > longestDiagonal) {
+            longestDiagonal = diagonal;
+        }
+    }
+    return longestDiagonal;
+}
 
-export function cuboidDiagonal() {}
+export function cuboidDiagonal(cuboids: Cuboid[]) {
+    if (cuboids.length === 0) {
+        throw "The list of cuboids cannot be empty!";
+    }
+    let longestCuboidDiagonal = 0;
+    for (let cuboid of cuboids) {
+        const diagonal = Math.hypot(cuboid.height, cuboid.width, cuboid.depth);
+        if (diagonal > longestCuboidDiagonal) {
+            longestCuboidDiagonal = diagonal;
+        }
+    }
+    return longestCuboidDiagonal;
+}
--- main.ts
+++ main.ts
@@ -61,6 +61,40 @@
 
 // console.log (getPokemonByType("fire"));
 
-import { rectangleArea } from "./algebra";
-import { rectangleDiagonal } from "./algebra";
-import { cuboidDiagonal } from "./algebra";
+import { rectangleArea, rectangleDiagonal, cuboidDiagonal } from "./algebra";
+import type { Rectangle, Cuboid } from "./algebra";
+
+const rectangles: Rectangle[] = [
+    { width: 3, height: 3 },
+    { width: 69, height: 69 },
+    { width: 12, height: 12 },
+];
+const cuboids: Cuboid[] = [
+    { width: 4, height: 9, depth: 3 },
+    { width: 14, height: 69, depth: 30 },
+    { width: 3, height: 5, depth: 14 },
+];
+try {
+    const rectangleReturn = rectangleArea(rectangles);
+    console.log(`The biggest rectangle has an area of ${rectangleReturn}cm^2.`);
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    const rectangleReturn = rectangleDiagonal(rectangles);
+    console.log(
+        `The longest diagonal of the given rectangles has a lenght of ${rectangleReturn}cm.`
+    );
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    const cuboidReturn = cuboidDiagonal(cuboids);
+    console.log(
+        `The longest diagonal of the given cuboids has a lenght of ${cuboidReturn}cm.`
+    );
+} catch (error) {
+    console.error(error);
+}
