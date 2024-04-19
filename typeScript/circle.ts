985abe13f8c39f78bbc08004c124dbe3834ec1ab
Marvin <maples4000@googlemail.com>
2024-04-19 12:40:34 +0200

    Created my first website with HTML. It shows an example of my CV.

--- typeScript/circle.ts
+++ typeScript/circle.ts
@@ -1,7 +1,7 @@
 const radius = 8;
 const pi = 3.14;
 export function circle(radius: number) {
-    const circumference = (pi*2)*radius;
+    const circumference = pi * 2 * radius;
     return `Ein Kreis mit einem Radius von ${radius} hat einen Umfang von ${circumference}.`;
 }
 //const result = circle(radius);
