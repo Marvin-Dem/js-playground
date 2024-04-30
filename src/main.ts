1ab0b44047519432e67ee17bbf8e86b5accdde9d
Marvin <maples4000@googlemail.com>
2024-04-30 15:09:08 +0200

    #19 Built the component "Hello World"

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-type Person = {
-    givenName: string;
-    lastName: string;
-};
-const person: Person = {
-    givenName: "Penis",
-    lastName: "haaaaalaloooallalo1234",
-};
-console.log(person);
+// type Person = {
+//     givenName: string;
+//     lastName: string;
+// };
+// const person: Person = {
+//     givenName: "Penis",
+//     lastName: "haaaaalaloooallalo1234567",
+// };
+// console.log(person);
+const element = document.getElementById("root")!;
+const hello = HelloWorld();
+function HelloWorld() {
+    const div = document.createElement("div");
+    div.innerText = "Hello World";
+    const list = div.classList;
+    list.add("hello-world");
+    return div;
+}
+element.appendChild(hello);
