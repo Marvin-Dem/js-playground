5f147ed16234d0d227cba34124aefd512ca8199d
Marvin <maples4000@googlemail.com>
2024-06-06 16:12:15 +0200

    React  bugfixing attempt.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/pokemon.ts",
+    entry: "./src/react.tsx",
     devtool: "source-map",
     module: {
         rules: [
@@ -16,7 +16,7 @@ module.exports = {
         extensions: [".tsx", ".ts", ".js"],
     },
     output: {
-        filename: "pokemon.js",
+        filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
 };
